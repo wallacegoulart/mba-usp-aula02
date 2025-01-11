92823404e3909cb0b7098faeae738d646ac6ecfb
Wallace Goulart <75147050+wallacegoulart@users.noreply.github.com>
2025-01-11 13:23:48 +0000

    projeto com javascripts

--- src/primeiroProjeto/app.js
+++ src/primeiroProjeto/app.js
@@ -1,1 +1,19 @@
-console.log(document)
+let botao = document.querySelector("button")
+console.log(botao)
+botao.addEventListener("click",somarUm)
+
+function somarUm(){
+    let elementoH3 = document.querySelector("h3")
+    let valor = parseInt(elementoH3.textContent)
+    let novoValor = valor + 1
+    elementoH3.textContent = novoValor
+
+
+    let r = parseInt(Math.random()*256)
+    let g = parseInt(Math.random()*256)
+    let b = parseInt(Math.random()*256)
+
+    if (novoValor > valor){
+        elementoH3.style.color = `rgb(${r},${g},${b})`
+    }
+}
